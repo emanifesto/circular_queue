a7f76187df2991ac57c566ff2318b7476ccdd537
emanifesto <emmanuelbabayemi37@gmail.com>
2025-12-19 19:17:35 -0500

    ring buffer struct with head and tail initialized

--- main.cpp
+++ main.cpp
@@ -18,3 +18,16 @@ struct padded_atomic_size {
     atomic_size_t value;
     char padding[CACHE_LINE - sizeof(atomic_size_t)];
 };
+
+struct spsc_queue {
+    struct ring_buffer buffer;
+
+    struct padded_atomic_size head;
+
+    struct padded_atomic_size tail;
+};
+
+void spsc_init(struct spsc_queue *q) {
+    atomic_init(&q->head.value, 0);
+    atomic_init(&q->tail.value, 0);
+}
