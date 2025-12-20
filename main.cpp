1e306bd813fbae5cb09b1cc7b823856c16a7129e
emanifesto <emmanuelbabayemi37@gmail.com>
2025-12-19 21:08:00 -0500

    implemented single producer, multiple consumer pop and push

--- main.cpp
+++ main.cpp
@@ -60,6 +60,44 @@ int spsc_pop(struct spsc_queue *q, struct item *out) {
     return 1;
 }
 
+//same as single producer, single consumer
+int spmc_push(struct spsc_queue *q, struct item item) {
+    size_t tail = atomic_load_explicit(&q->tail.value, memory_order_relaxed);
+
+    size_t next_tail = (tail + 1) & MASK;
+
+    if (next_tail == atomic_load_explicit(&q->head.value, memory_order_acquire)) {
+        return 0;
+    }
+
+    q->buffer.data[tail] = item;
+
+    atomic_store_explicit(&q->tail.value, next_tail, memory_order_release);
+    return 1;
+}
+
+int spmc_pop(struct spsc_queue *q, struct item *out) {
+    size_t head;
+
+    while (1) {
+        head = atomic_load_explicit(&q->head.value, memory_order_relaxed);
+
+        size_t tail = atomic_load_explicit(&q->tail.value, memory_order_acquire);
+
+        if (head == tail) {
+            return 0;
+        }
+
+        size_t next_head = (head + 1) & MASK;
+
+        if (atomic_compare_exchange_weak_explicit(&q->head.value, &head, next_head, memory_order_acquire, memory_order_relaxed)) {
+            break;
+        }
+    }
+    *out = q->buffer.data[head];
+    return 1;
+}
+
 int main(void){
     return 0;
 }
